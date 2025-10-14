d368f302a56a1fe6db10510f67dfea7016eb2ba1
dalianassar <daliayassernassar@gmail.com>
2025-10-14 15:57:18 +0300

    add

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,7 +1,7 @@
 
 const x = Number(process.argv[2]);
 
-if (!x || x < 1 || isNaN(x)) {
+if (!process.argv[2] || isNaN(x)) {
     console.log("Missing number of occurrences");
 } else {
     let i = 0;
